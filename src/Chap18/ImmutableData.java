19a7d026e98ca79cb8e4837b48fd28e90a8a574d
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2025-10-30 11:25:36 +0200

    Chap 18 RyanAndMonica Amendments

---
+++ src/Chap18/ImmutableData.java
@@ -0,0 +1,15 @@
+package Chap18;
+
+public class ImmutableData {
+  final String name;
+  final int value;
+
+  public ImmutableData(String name, int value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getName() { return name; }
+
+  public int getValue() { return value; }
+}
